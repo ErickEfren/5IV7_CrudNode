099d8d9d860ec05dc804167155d7d3254329b6c7
ErickEfren <erosalesb2000@alumno.ipn.mx>
2022-10-14 00:20:53 -0500

    CrudNode

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 7526;
-app.set('view engine', 'ejs');
+app.set('view engine', '.ejs');
 app.use(express.urlencoded({extended:false}));
 app.use('/', require('./router.js'));
 app.listen(PORT, ()=>{
